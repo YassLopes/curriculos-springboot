d608d9aa8261cc231b0c86d0b82652975b86d5ed
Yasmin Lopes <148560159+YassLopes@users.noreply.github.com>
2025-04-20 17:19:43 -0300

    Delete init-db.ps1

--- init-db.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Waiting for PostgreSQL to start..."
-while (-not (Test-NetConnection -ComputerName localhost -Port 5432 -InformationLevel Quiet)) {
-    Start-Sleep -Seconds 1
-}
-Write-Host "PostgreSQL is ready!" 
